1fc550a534fd9b2c14a0e8e220184e589cdbbae7
wengel-hm <william.engel@mdynamix.de>
2024-07-17 11:12:32 +0200

    add multi threading

--- src/slowroads_sim.py
+++ src/slowroads_sim.py
@@ -1,29 +1,110 @@
-from slowroads_utils import open_browser, grab_screenshot, set_cruise_speed, autodrive_on, autodrive_off
+from slowroads_utils import open_browser, grab_screenshot, set_cruise_speed, autodrive_on, autodrive_off, save_screenshot, KeyListener
 import matplotlib.pyplot as plt
 from slowroads_utils import steer_left, steer_right
+import time
+import threading
+import signal
+
+# sim.init_control_thread()
+# sim.pause_control_thread()
+# sim.run_control_thread()
 
 class SlowRoadsSimulator:
     def __init__(self):
-        self.init_plot()
 
-    def init_plot(self):
+        self.plot_initialized = False
+        self.control_initialized = False
+        self.key_listener_initialized = False
+        self.driver_initialized = False
+
+        # Thread event to signal exit
+        self.exit_event = threading.Event()
+        # Thread event to run the controller
+        self.run_event = threading.Event()
+
+        # Set up signal handling for graceful exit
+        signal.signal(signal.SIGINT, self.signal_handler)
+
+    def signal_handler(self, signum, frame):
+        """Handle SIGINT to set exit event."""
+        self.exit_event.set()
+
+    def __clear__(self):
+
+            if self.control_initialized:
+                print("Initiating shutdown of the control thread...")
+                self.exit_event.set()  # Signal the control thread to exit
+                self.control_thread.join()  # Wait for the control thread to exit
+                print("Control thread shutdown successfully completed.")
+
+            if self.plot_initialized:
+                print("Initiating closure sequence for the plot...")
+                plt.close('all')  # This ensures that all figures are closed properly.
+                print("Plot closure completed.")
+
+            if self.key_listener_initialized:
+                print("Initiating shutdown of the KeyListener.")
+                self.key_listener.stop_listening()
+                print("KeyListener shutdown successfully completed.")
+
+            if self.driver_initialized:
+                print("Shutting down the driver...")
+                self.driver.quit()
+                print("Driver shutdown complete. All resources have been cleaned up successfully.")
+
+    def init_control_thread(self):
+        self.run_event.clear()  # Start in an paused state
+        # Background thread to send commands periodically
+        self.control_thread = threading.Thread(target=self._publish_commands)
+        self.control_thread.daemon = True
+        self.control_thread.start()
+        self.control_initialized = True
+
+    def run_control_thread(self):
+        self.run_event.set() # Running state
+
+    def pause_control_thread(self):
+        self.run_event.clear() # Paused state
+
+    def is_paused(self):
+        return self.run_event.is_set()
+    
+    def _publish_commands(self):
+
+        """Thread function to send commands at fixed intervals."""
+        while not self.exit_event.is_set():
+            
+            if not self.run_event.is_set(): # Wait until run_event is set
+                time.sleep(1)
+            else:
+                self.publish_commands()
+
+    def publish_commands(self):
+        print("Publishing Control commands")
+        time.sleep(0.5)
+
+    def update_plot(self, image = None, stats_dict = None):
+
+        if not self.plot_initialized:
+            self._init_plot()
+
+        if not image is None:
+            self._plot_image(image)
+
+        if not stats_dict is None:
+            self._plot_stats(stats_dict)
+
+        plt.draw()
+        plt.pause(0.01)  # Adjust pause time as needed
+
+    def _init_plot(self):
         plt.ion()  # Turn on interactive mode
         _, self.ax = plt.subplots()
         self.display = None
         self.text_obj = None
+        self.plot_initialized = True
 
-    def update_plot(self, image = None, stats_dict = None):
-
-        if not image is None:
-            self.plot_image(image)
-
-        if not stats_dict is None:
-            self.plot_stats(stats_dict)
-
-        plt.draw()
-        plt.pause(0.01)  # Adjust pause time as needed
-    
-    def plot_image(self, image):
+    def _plot_image(self, image):
         """Update the image displayed on the Axes."""
         if self.display is None:
             # Create the display object if it doesn't exist
@@ -32,7 +113,7 @@ class SlowRoadsSimulator:
             # Update the display object with the new image
             self.display.set_data(image)
 
-    def plot_stats(self, stats_dict):
+    def _plot_stats(self, stats_dict):
         """Plot statistics on the image like a legend, using a dictionary of values."""
         # Generate the statistics text from the dictionary
         stats_text = "\n".join(f"{key}: {value}" for key, value in stats_dict.items())
@@ -49,8 +130,10 @@ class SlowRoadsSimulator:
 
         
     def open_brwoser(self, local_storage_path = None, size = (640, 360)):
-        # Open SlowRoads in Chrome Browser
-        self.driver = open_browser(local_storage_path, size)
+        if not self.driver_initialized:
+            # Open SlowRoads in Chrome Browser
+            self.driver = open_browser(local_storage_path, size)
+            self.driver_initialized = True
 
     def set_speed(self, speed):
         set_cruise_speed(self.driver, speed)
@@ -64,27 +147,36 @@ class SlowRoadsSimulator:
     def grab_screenshot(self):
         return grab_screenshot(self.driver)
 
+    def save_screenshot(self, directory = None):
+        save_screenshot(self.driver, directory)
+
     def autodrive_on(self):
         autodrive_on(self.driver)
     
     def autodrive_off(self):
         autodrive_off(self.driver)
 
+    def rest_vehicle(self, t = 2):
+        self.autodrive_on()
+        time.sleep(t)
+        self.autodrive_off()
+        
+    def add_key_action(self, key, func):
+        # Unassigned keys: G,J,L,N,O,X,Y
+
+        if not self.key_listener_initialized:
+            self.key_listener = KeyListener()
+            self.key_listener_initialized = True
+
+        self.key_listener.add_key_action(key, func)
+
     def run(self):
 
         """Main loop to capture images and update commands."""
         try:
-            while True:
-                image = self.grab_screenshot()
-
-        except KeyboardInterrupt:
-            print("Operation terminated: Request received from user.")
+            while not self.exit_event.is_set():
+                time.sleep(0.1)
+                # image = self.grab_screenshot()
         finally:
-            print("Initiating closure sequence for the plot...")
-            plt.close('all')  # This ensures that all figures are closed properly.
-            print("Plot closure completed.")
-
-            print("Shutting down the driver...")
-            self.driver.quit()
-            print("Driver shutdown complete. All resources have been cleaned up successfully.")
+            self.__clear__()
 
--- src/slowroads_utils.py
+++ src/slowroads_utils.py
@@ -10,6 +10,8 @@ from selenium.webdriver.common.by import By
 from datetime import datetime
 import os
 from selenium.common.exceptions import NoSuchElementException, WebDriverException
+from PIL import Image
+import io
 
 # Free Keys
 # G,J,L,N,O,X,Y
@@ -57,6 +59,8 @@ def open_browser(local_storage_path = None, size = (640, 360)):
 class KeyListener:
     def __init__(self):
         self.key_actions = {}  # Dictionary to store key-specific functions
+        self.listener = None   # Listener object, initially None
+        self.is_listening = False  # Flag to track if the listener has started
 
     def add_key_action(self, key, action):
         """Adds a custom action for a specific key.
@@ -66,13 +70,15 @@ class KeyListener:
             action: The function to be executed when the key is pressed.
         """
         self.key_actions[key] = action
+        # Start listening when the first key action is added
+        if not self.is_listening:
+            self.start_listening()
 
     def on_press(self, key):
-
         try:
             # Check if the pressed key has a registered action
             if hasattr(key, 'char') and key.char in self.key_actions:
-                self.key_actions[key.char]() # Call action
+                self.key_actions[key.char]()  # Call action
         except AttributeError:
             pass
 
@@ -80,12 +86,15 @@ class KeyListener:
         pass
         
     def start_listening(self):
-        self.listener = Listener(on_press=self.on_press, on_release=self.on_release)
-        self.listener.start()
+        if not self.is_listening:
+            self.listener = Listener(on_press=self.on_press, on_release=self.on_release)
+            self.listener.start()
+            self.is_listening = True  # Set flag to True as the listener has started
 
     def stop_listening(self):
         if self.listener is not None:
             self.listener.stop()
+            self.is_listening = False  # Reset flag as the listener has stopped
 
         
 def load_local_storage(driver, path):
@@ -119,6 +128,10 @@ def open_safari():
 
 
 def save_screenshot(driver, directory):
+
+    # Check if directory is specified and is not None
+    if not directory:
+        raise ValueError("A directory must be specified to save the screenshot.")
     
     now = datetime.now()
     filename = now.strftime("image_%Y%m%d%H%M%S.png")
@@ -129,28 +142,55 @@ def save_screenshot(driver, directory):
 
 @track_time            
 def grab_screenshot(driver):
-    screenshot = driver.get_screenshot_as_png()
-    nparr = np.frombuffer(screenshot, np.uint8)
-    image = cv.imdecode(nparr, cv.IMREAD_COLOR)
+    try:
+        screenshot = driver.get_screenshot_as_png()
+        nparr = np.frombuffer(screenshot, np.uint8)
+        image = cv.imdecode(nparr, cv.IMREAD_COLOR)
+    except WebDriverException as e:
+        print(f"WebDriverException occurred in control thread: {e}")
+        # Handle WebDriver-related errors
+    except Exception as e:
+        print(f"An unexpected error occurred in control thread: {e}")
+        # Handle other unexpected exceptions
+
     return image
-            
+
+@track_time     
 def steer_left(driver, t_down = 0.2, t_up = 0.1):
-    action = ActionChains(driver)
-
-    # Press and hold the left key
-    action.key_down(Keys.ARROW_LEFT).pause(t_down)
-    action.key_up(Keys.ARROW_LEFT).pause(t_up)
     
-    action.perform()
+    try:
+        action = ActionChains(driver)
 
+        # Press and hold the left key
+        action.key_down(Keys.ARROW_LEFT).pause(t_down)
+        action.key_up(Keys.ARROW_LEFT).pause(t_up)
+        
+        action.perform()
+
+    except WebDriverException as e:
+        print(f"WebDriverException occurred in control thread: {e}")
+        # Handle WebDriver-related errors
+    except Exception as e:
+        print(f"An unexpected error occurred in control thread: {e}")
+        # Handle other unexpected exceptions
+
+@track_time
 def steer_right(driver, t_down = 0.2, t_up = 0.1):
-    action = ActionChains(driver)
-
-    # Press and hold the right key
-    action.key_down(Keys.ARROW_RIGHT).pause(t_down)
-    action.key_up(Keys.ARROW_RIGHT).pause(t_up)
     
-    action.perform()
+    try:
+        action = ActionChains(driver)
+
+        # Press and hold the right key
+        action.key_down(Keys.ARROW_RIGHT).pause(t_down)
+        action.key_up(Keys.ARROW_RIGHT).pause(t_up)
+        
+        action.perform()
+    except WebDriverException as e:
+        print(f"WebDriverException occurred in control thread: {e}")
+        # Handle WebDriver-related errors
+    except Exception as e:
+        print(f"An unexpected error occurred in control thread: {e}")
+        # Handle other unexpected exceptions
 
 def set_value(driver, id_name, value):
     # Execute the script
